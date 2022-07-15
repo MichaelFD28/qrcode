f06aae80c037c9304478c66f18d61e58f883ad67
Michael Driscoll <mfdriscoll28@gmail.com>
2022-07-15 11:21:52 +0100

    used html5qrcode instead

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import QRCode from "qrcode";
-import { QrReader } from "react-qr-reader";
 import Container from "@mui/material/Container";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -9,13 +8,14 @@ import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
+import HowToUse from "./HowToUse.jsx";
+import Html5QrcodePlugin from "./Html5QrcodePlugin.jsx";
+import ResultContainerPlugin from "./ResultContainerPlugin.jsx";
+
 function App() {
   const [link, setLink] = useState("");
   const [imageURL, setImageURL] = useState("");
-  const [errorType, setErrorType] = useState("");
-  // const [scanResultFile, setScanResultFile] = useState("");
-  const [scanResultWebCam, setScanResultWebCam] = useState("");
-  // const qrRef = useRef(null);
+  const [decodedResults, setDecodedResults] = useState([]);
 
   const generateQrCode = async () => {
     try {
@@ -27,28 +27,9 @@ function App() {
     }
   };
 
-  // const handleScanFile = (result, error) => {
-  //   if (result) {
-  //     setScanResultFile(result?.text);
-  //   }
-  //   if (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // const onScanFile = () => {
-  //   // `current` points to the mounted text input element
-  //   qrRef.current?.openImageDialog();
-  // };
-
-  const handleScanWebCam = (result, error) => {
-    if (result) {
-      setScanResultWebCam(result?.text);
-    }
-    if (error) {
-      console.log(error);
-      setErrorType(error);
-    }
+  const onNewScanResult = (decodedText, decodedResult) => {
+    console.log("App [result]", decodedResult);
+    setDecodedResults((prev) => [...prev, decodedResult]);
   };
 
   return (
@@ -59,7 +40,7 @@ function App() {
         </h2>
         <CardContent>
           <Grid container spacing={4}>
-            <Grid item xl={4} lg={4} m={6} sm={12} xs={12}>
+            <Grid item xl={5} lg={5} m={6} sm={12} xs={12}>
               <TextField
                 label="Enter Link Here"
                 variant="standard"
@@ -86,46 +67,16 @@ function App() {
                 </a>
               )}
             </Grid>
-            {/* <Grid item xl={4} lg={4} m={6} sm={12} xs={12}>
-              <Button
-                className="btn"
-                variant="contained"
-                color="secondary"
-                onClick={onScanFile}
-              >
-                Scan Qr Code
-              </Button>
-              <QrReader
-                ref={qrRef}
-                scanDelay={300}
-                style={{ width: "100%", border: "solid 2px #a8a7a7" }}
-                onResult={handleScanFile}
+            <Grid item xl={5} lg={5} m={6} sm={12} xs={12}>
+              <Html5QrcodePlugin
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}
               />
-              <h3>Scanned Code: {scanResultFile}</h3>
-            </Grid> */}
-
-            <Grid item xl={4} lg={4} m={6} sm={12} xs={12}>
-              <h3>QR Code Scan by Web Cam</h3>
-              {/* {errorType && <div className="error">Camera not found.</div>} */}
-              <QrReader
-                onResult={(result, error) => {
-                  if (!!result) {
-                    setScanResultWebCam(result?.text);
-                  }
-
-                  if (!!error) {
-                    console.info(error);
-                  }
-                }}
-                style={{ width: "100%" }}
-              />
-              {/* <QrReader
-                delay={300}
-                style={{ width: "100%" }}
-                onResult={handleScanWebCam}
-              /> */}
-              <h3>Scanned Code: {scanResultWebCam}</h3>
+              <ResultContainerPlugin results={decodedResults} />
             </Grid>
+            {/* <HowToUse /> */}
           </Grid>
         </CardContent>
       </Card>
